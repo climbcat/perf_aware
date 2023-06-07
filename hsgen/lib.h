366336a5694da3035df73eb0839c9235ce76b647
climbcat <jakob.garde@gmail.com>
2023-06-07 15:38:59 +0200

    code cleanup

--- hsgen/lib.h
+++ hsgen/lib.h
@@ -1,8 +1,9 @@
-#ifndef __LIB_H__
-#define __LIB_H__
-
-#include <sys/time.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
 #include <cstdint>
+#include <sys/time.h>
+#include "math.h"
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -13,6 +14,8 @@ typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
+
+typedef double f64;
 
 #define KILOBYTE 1024
 #define MEGABYTE 1024 * 1024
@@ -88,4 +91,44 @@ double RandPM1() {
     return randnum;
 }
 
-#endif
+
+// programming aware stuff:
+
+
+static f64 Square(f64 A)
+{
+    f64 Result = (A*A);
+    return Result;
+}
+
+static f64 Deg2Rad(f64 Degrees)
+{
+    f64 Result = 0.01745329251994329577f * Degrees;
+    return Result;
+}
+
+// NOTE(casey): EarthRadius is generally expected to be 6372.8
+static f64 ReferenceHaversine(f64 X0, f64 Y0, f64 X1, f64 Y1, f64 EarthRadius)
+{
+    /* NOTE(casey): This is not meant to be a "good" way to calculate the Haversine distance.
+       Instead, it attempts to follow, as closely as possible, the formula used in the real-world
+       question on which these homework exercises are loosely based.
+    */
+
+    f64 lat1 = Y0;
+    f64 lat2 = Y1;
+    f64 lon1 = X0;
+    f64 lon2 = X1;
+    
+    f64 dLat = Deg2Rad(lat2 - lat1);
+    f64 dLon = Deg2Rad(lon2 - lon1);
+    lat1 = Deg2Rad(lat1);
+    lat2 = Deg2Rad(lat2);
+    
+    f64 a = Square(sin(dLat/2.0)) + cos(lat1)*cos(lat2)*Square(sin(dLon/2));
+    f64 c = 2.0*asin(sqrt(a));
+    
+    f64 Result = EarthRadius * c;
+    
+    return Result;
+}
--- hsgen/main.cpp
+++ hsgen/main.cpp
@@ -1,56 +1,9 @@
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-
 #include "lib.h"
-#include "math.h"
-
-typedef double f64;
-typedef uint32_t u32;
-
-
-static f64 Square(f64 A)
-{
-    f64 Result = (A*A);
-    return Result;
-}
-
-static f64 Deg2Rad(f64 Degrees)
-{
-    f64 Result = 0.01745329251994329577f * Degrees;
-    return Result;
-}
-
-// NOTE(casey): EarthRadius is generally expected to be 6372.8
-static f64 ReferenceHaversine(f64 X0, f64 Y0, f64 X1, f64 Y1, f64 EarthRadius)
-{
-    /* NOTE(casey): This is not meant to be a "good" way to calculate the Haversine distance.
-       Instead, it attempts to follow, as closely as possible, the formula used in the real-world
-       question on which these homework exercises are loosely based.
-    */
-
-    f64 lat1 = Y0;
-    f64 lat2 = Y1;
-    f64 lon1 = X0;
-    f64 lon2 = X1;
-    
-    f64 dLat = Deg2Rad(lat2 - lat1);
-    f64 dLon = Deg2Rad(lon2 - lon1);
-    lat1 = Deg2Rad(lat1);
-    lat2 = Deg2Rad(lat2);
-    
-    f64 a = Square(sin(dLat/2.0)) + cos(lat1)*cos(lat2)*Square(sin(dLon/2));
-    f64 c = 2.0*asin(sqrt(a));
-    
-    f64 Result = EarthRadius * c;
-    
-    return Result;
-}
 
 void CreateHaversinePointsJson(u32 num_points, u32 seed) {
 
     FILE *file_json = fopen("hspairs.json", "w");
-    FILE *file_bin = fopen("hsdist_answers.bin", "w");
+    FILE *file_bin = fopen("hsdist.bin", "w");
 
     seed = RandInit(seed);
     f64 x0, y0, x1, y1, hsdist, hsdist_sum = 0, earth_radius = 6372.8;
@@ -71,9 +24,6 @@ void CreateHaversinePointsJson(u32 num_points, u32 seed) {
         // calculate control answer
         hsdist = ReferenceHaversine(x0, y0, x1, y1, earth_radius);
         hsdist_sum += hsdist;
-
-        // DEBUG: 
-        //printf("%.16f\n", hsdist);
 
         // write pair to json
         fprintf(file_json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}", x0, y0, x1, y1);
