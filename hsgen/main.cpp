a684638da5f2321bbea8b40934dc14254cbfa728
climbcat <jakob.garde@gmail.com>
2023-06-08 08:22:56 +0200

    usability, --sectors cmd line arg

--- hsgen/main.cpp
+++ hsgen/main.cpp
@@ -1,17 +1,33 @@
 #include "lib.h"
 
-void CreateHaversinePointsJson(u32 num_points, u32 seed) {
 
-    FILE *file_json = fopen("hspairs.json", "w");
-    FILE *file_bin = fopen("hsdist.bin", "w");
+struct HsPair {
+    f64 x0 = 0;
+    f64 y0 = 0;
+    f64 x1 = 0;
+    f64 y1 = 0;
+};
+
+void CreateHaversinePointsJson(u32 npoints, u32 seed, bool sectors) {
+
+    const char *filename_json = "hspairs.json";
+    const char *filename_bin = "hsdist.bin";
+    FILE *file_json = fopen(filename_json, "w");
+    FILE *file_bin = fopen(filename_bin, "w");
 
     seed = RandInit(seed);
     f64 x0, y0, x1, y1, hsdist, hsdist_sum = 0, earth_radius = 6372.8;
 
-    // TODO: do sector setup here (16 square sectors, 4 x and 4 y sectors, 3 randon x and y values as separators)
-
+    f64 sector_x[3];
+    f64 sector_y[3];
+    f64 sector_sum[3*3];
+    for (int i = 0; i < 4; ++i) {
+        sector_x[i] = 180.0f * RandPM1();
+        sector_y[i] = 90.0f * RandPM1();
+    }
+    
     fprintf(file_json, "{\"pairs\":[\n");
-    for (int i = 0; i < num_points; ++i) {
+    for (int i = 0; i < npoints; ++i) {
 
         // TODO: select a sector (two random ints from 0-3 to hit x, y sector)
 
@@ -24,10 +40,11 @@ void CreateHaversinePointsJson(u32 num_points, u32 seed) {
         // calculate control answer
         hsdist = ReferenceHaversine(x0, y0, x1, y1, earth_radius);
         hsdist_sum += hsdist;
-
+        
         // write pair to json
-        fprintf(file_json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}", x0, y0, x1, y1);
-        if (i + 1 < num_points) {
+        fprintf(file_json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}",
+                x0, y0, x1, y1);
+        if (i + 1 < npoints) {
             fprintf(file_json, ",");
         }
         fprintf(file_json, "\n");
@@ -40,7 +57,12 @@ void CreateHaversinePointsJson(u32 num_points, u32 seed) {
     fclose(file_json);
     fclose(file_bin);
 
-    printf("Haversine distance data generated from seed=%u:\n    sum = %.16f\n    average = %.16f\n", seed, hsdist_sum, hsdist_sum / num_points);
+    printf("Haversine distance data generated using\n    seed:    %u\n    npoints: %u\n",
+           seed, npoints);
+    printf("Yeilding\n    sum:     %.16f\n    average: %.16f\n",
+           hsdist_sum, hsdist_sum / npoints);
+    printf("Pairs data file: %s\n", filename_json);
+    printf("Haversine answers file: %s\n", filename_bin);    
 }
 
 void Test() {
@@ -75,11 +97,19 @@ u32 ParseInt(char *text) {
 }
 
 int main (int argc, char **argv) {
+    if (ContainsArg("--help", argc, argv) || ContainsArg("-h", argc, argv)) {
+        printf("--help:          display help (this text)\n--test:          run tests and exit\n--sectors:       fragment points into sectors\n--npoits [int]:  number of points to generate\n--seed [int]:    set random seed\n");
+        exit(0);
+    }
     if (ContainsArg("--test", argc, argv)) {
         Test();
         exit(0);
     }
 
+    bool sectors = false;
+    if (ContainsArg("--sectors", argc, argv)) {
+        sectors = true;
+    }
     u32 seed = 0;
     if (ContainsArg("--seed", argc, argv)) {
         seed = ParseInt(GetArgValue("--seed", argc, argv));
@@ -89,8 +119,5 @@ int main (int argc, char **argv) {
         npoints = ParseInt(GetArgValue("--npoints", argc, argv));
     }
 
-    // TODO: cmd line args:
-    //      --sectors
-
-    CreateHaversinePointsJson(npoints, seed);
+    CreateHaversinePointsJson(npoints, seed, sectors);
 }
