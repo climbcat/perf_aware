a7b328502b1c2c17eea58c93c262bc1298c0d9a8
climbcat <jakob.garde@gmail.com>
2023-06-19 20:26:36 +0200

    defer impl. of sectors, re-enable base functionality

--- hsgen/main.cpp
+++ hsgen/main.cpp
@@ -45,85 +45,84 @@ void CreateHaversinePointsJson(u32 npoints, u32 seed, bool use_sectors) {
     f64 hsdist_sum = 0;
     f64 earth_radius = 6372.8;
     HsPair p;
-    Sector s;
 
-    u8 num_sectors_1d = 4;
+    if (use_sectors) {
+        Sector s;
+        
+        u8 num_sectors_1d = 4;
+        u32 sclen = num_sectors_1d + 1;
+        f64 split_x[sclen];
+        f64 split_y[sclen];
+        split_x[0] = -180.0;
+        split_x[sclen-1] = 180.0;
+        split_y[0] = -90.0;
+        split_y[sclen-1] = 90.0;
+        for (int i = 1; i < sclen - 1; ++i) {
+            split_x[i] = 180.0f * RandPM1();
+            split_y[i] = 90.0f * RandPM1();
+        }
+        printf("split_x: %f %f %f %f %f\n", split_x[0], split_x[1], split_x[2], split_x[3], split_x[4]);
+        printf("split_y: %f %f %f %f %f\n\n", split_y[0], split_y[1], split_y[2], split_y[3], split_y[4]);
+        // sort split values
+        f64 swap;
 
-    u32 sclen = num_sectors_1d + 1;
-    f64 split_x[sclen];
-    f64 split_y[sclen];
-    split_x[0] = -180.0;
-    split_x[sclen-1] = 180.0;
-    split_y[0] = -90.0;
-    split_y[sclen-1] = 90.0;
-    for (int i = 1; i < sclen - 1; ++i) {
-        split_x[i] = 180.0f * RandPM1();
-        split_y[i] = 90.0f * RandPM1();
-    }
-    printf("split_x: %f %f %f %f %f\n", split_x[0], split_x[1], split_x[2], split_x[3], split_x[4]);
-    printf("split_y: %f %f %f %f %f\n\n", split_y[0], split_y[1], split_y[2], split_y[3], split_y[4]);
-    // sort split values
-    f64 swap;
-
-    u32 splitters = 5;
-    for (int j = 0; j < sclen - 1; ++j) {
-        for (int i = 0; i < sclen - 2 - j; ++i) {
+        u32 splitters = 5;
+        for (int j = 0; j < sclen - 1; ++j) {
+            for (int i = 0; i < sclen - 2 - j; ++i) {
             
-            if (split_x[i] > split_x[i+1]) {
-                swap = split_x[i];
-                split_x[i] = split_x[i+1];
-                split_x[i+1] = swap;
+                if (split_x[i] > split_x[i+1]) {
+                    swap = split_x[i];
+                    split_x[i] = split_x[i+1];
+                    split_x[i+1] = swap;
+                }
+                if (split_y[i] > split_y[i+1]) {
+                    swap = split_y[i];
+                    split_y[i] = split_y[i+1];
+                    split_y[i+1] = swap;
+                }
             }
-            if (split_y[i] > split_y[i+1]) {
-                swap = split_y[i];
-                split_y[i] = split_y[i+1];
-                split_y[i+1] = swap;
+        }
+        printf("split_x: %f %f %f %f %f\n", split_x[0], split_x[1], split_x[2], split_x[3], split_x[4]);
+        printf("split_y: %f %f %f %f %f\n\n", split_y[0], split_y[1], split_y[2], split_y[3], split_y[4]);
+
+        printf("sectors:\n");
+        Sector sectors[sclen * sclen];
+        for (int i = 0; i < sclen - 1; ++i) {
+            for (int j = 0; j < sclen - 1; ++j) {
+                s.xmin = split_x[i];
+                s.xmax = split_x[i+1];
+                s.ymin = split_y[j];
+                s.ymax = split_y[j+1];
+
+                sectors[num_sectors_1d*j + i] = s;
+            }
+        }
+        for (int i = 0; i < sclen - 1; ++i) {
+            for (int j = 0; j < sclen - 1; ++j) {
+                PrintSector(sectors[num_sectors_1d*j + i]);
             }
         }
     }
-    printf("split_x: %f %f %f %f %f\n", split_x[0], split_x[1], split_x[2], split_x[3], split_x[4]);
-    printf("split_y: %f %f %f %f %f\n\n", split_y[0], split_y[1], split_y[2], split_y[3], split_y[4]);
-
-    printf("sectors:\n");
-    Sector sectors[sclen * sclen];
-    for (int i = 0; i < sclen - 1; ++i) {
-        for (int j = 0; j < sclen - 1; ++j) {
-            s.xmin = split_x[i];
-            s.xmax = split_x[i+1];
-            s.ymin = split_y[j];
-            s.ymax = split_y[j+1];
-
-            sectors[num_sectors_1d*j + i] = s;
-        }
-    }
-    for (int i = 0; i < sclen - 1; ++i) {
-        for (int j = 0; j < sclen - 1; ++j) {
-            PrintSector(sectors[num_sectors_1d*j + i]);
-        }
-    }
-    exit(0);
 
     
     fprintf(file_json, "{\"pairs\":[\n");
+    Sector s;
     for (int i = 0; i < npoints; ++i) {
 
         // select sector
         if (use_sectors) {
-            //xmin = sector_x[RandMinMaxI(0, 2)];
-            //xmax = 
+            printf("TODO: impl. sectors\n");
         }
 
-        f64 xmin, xmax, ymin, ymax;
         // calculate point pair
-        p = RandomPair(xmin, xmax, ymin, ymax);
+        p = RandomPair(s.xmin, s.xmax, s.ymin, s.ymax);
 
         // calculate control answer
         hsdist = ReferenceHaversine(p.x0, p.y0, p.x1, p.y1, earth_radius);
         hsdist_sum += hsdist;
         
         // write pair to json
-        fprintf(file_json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}",
-                p.x0, p.y0, p.x1, p.y1);
+        fprintf(file_json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}", p.x0, p.y0, p.x1, p.y1);
         if (i + 1 < npoints) {
             fprintf(file_json, ",");
         }
